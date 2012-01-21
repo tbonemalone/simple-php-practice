8a7efe66234e308bc3a114938b7089b92474fa3f
tbonemalone <tony.org@gmail.com>
2012-01-21 10:39:22 -0800

    more clean up

--- inc/db.inc.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	// just need to include this file in any file that needs credentials
-	define('DB_INFO', 'mysql:host=localhost;dbname=simple_blog');
-	define('DB_USER', 'root');
-	define('DB_PASS', 'root');
-?>
